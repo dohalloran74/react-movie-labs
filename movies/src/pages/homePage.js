5eb4179967f473fe172b580c1d416a552da9ffe8
deirdreOH <dohalloran@wit.ie>
2024-10-07 12:36:47 +0100

    Added routing and live dara from API

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";  
+import MovieList from "../components/movieList";
 import Grid from "@mui/material/Grid2";
-import MovieCard from '../components/movieCard'
 
 const HomePage = (props) => {
-  const movies = props.movies;
-    
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json);
+        return json.results;
+      })
+      .then((movies) => {
+        setMovies(movies);
+      });
+  }, []);
   return (
-  <Grid container>
+    <Grid container>
       <Grid size={12}>
-          <h1> HomePage </h1>
+        <h1> HomePage </h1>
       </Grid>
-      <Grid size={3}>
-        <MovieCard movie={movies[0]} />
+      <Grid container>
+        <MovieList movies={movies}></MovieList>
       </Grid>
     </Grid>
   );
